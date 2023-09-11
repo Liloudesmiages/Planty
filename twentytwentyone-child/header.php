eb1e64d3d1fafd0cb851a159aa223a3565bfaf27
Aurélie PARRET <audatika@hotmail.fr>
2023-09-11 11:31:50 +0200

    Indentation + Commentaire

--- twentytwentyone-child/header.php
+++ twentytwentyone-child/header.php
@@ -1,7 +1,7 @@
 <header>
     <div class="logo">
        <img src="<?php echo get_stylesheet_directory_uri(); ?>/logo.png" alt="logo Planty">
-       </div>
+    </div>
     <nav>
     <?php wp_nav_menu(array('theme_location' => 'header')); ?>
      </nav>
